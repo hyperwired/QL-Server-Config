8e0c68f18b3b0ff0541e7f6e8e0b360b928bc978
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-08 06:35:55 +1000

    fix tar command issue

--- minqlx-upgrade.sh
+++ minqlx-upgrade.sh
@@ -8,7 +8,7 @@ if [ $CURRENT_MINQLX_VERSION != $LATEST_MINQLX_VERSION ]; then
     echo "Upgrading minqlx from version $CURRENT_MINQLX_VERSION to $LATEST_MINQLX_VERSION..."
     sudo rm -rf ~/steamcmd/steamapps/common/qlds/minqlx.zip ~/steamcmd/steamapps/common/qlds/minqlx.so
     wget https://github.com/MinoMino/minqlx/releases/download/v0.0.4a/minqlx_v0.0.4a.tar.gz
-    tar -xvfz minqlx_v0.0.4a.tar.gz
+    tar xvzf minqlx_v0.0.4a.tar.gz
     cd minqlx_v0.0.4a
     mv minqlx.zip ~/steamcmd/steamapps/common/qlds/minqlx.zip
     mv minqlx.so ~/steamcmd/steamapps/common/qlds/minqlx.so
