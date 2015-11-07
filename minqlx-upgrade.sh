4f0074f20f78e5b925ab9b4a48fec1ac60b537f8
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-08 06:38:38 +1000

    added silencers

--- minqlx-upgrade.sh
+++ minqlx-upgrade.sh
@@ -6,10 +6,10 @@ LATEST_MINQLX_VERSION="0.0.4a"
 
 if [ $CURRENT_MINQLX_VERSION != $LATEST_MINQLX_VERSION ]; then
     echo "Upgrading minqlx from version $CURRENT_MINQLX_VERSION to $LATEST_MINQLX_VERSION..."
-    sudo rm -rf ~/steamcmd/steamapps/common/qlds/minqlx.zip ~/steamcmd/steamapps/common/qlds/minqlx.so
+    sudo rm -rf ~/steamcmd/steamapps/common/qlds/minqlx.zip ~/steamcmd/steamapps/common/qlds/minqlx.so > /dev/null
     mkdir temp; cd temp
     wget https://github.com/MinoMino/minqlx/releases/download/v0.0.4a/minqlx_v0.0.4a.tar.gz > /dev/null
-    tar xvzf minqlx_v0.0.4a.tar.gz
+    tar xvzf minqlx_v0.0.4a.tar.gz > /dev/null
     cd minqlx_v0.0.4a
     mv minqlx.zip ~/steamcmd/steamapps/common/qlds/minqlx.zip
     mv minqlx.so ~/steamcmd/steamapps/common/qlds/minqlx.so
