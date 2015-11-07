9f5bb908dae5945e07099ffe2a8a6d98376370b7
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-08 06:47:10 +1000

    remove existing minqlx plugins

--- pre-run.sh
+++ pre-run.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # Pre-run script
 
+# Upgrade minqlx if needed.
 bash minqlx-upgrade.sh
+
+# Remove all minqlx plugins
+rm -rf ~/steamcmd/steamapps/common/qlds/minqlx-plugins/*.py
 
 exit 0
