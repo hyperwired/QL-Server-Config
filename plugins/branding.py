f2db40f04bf82b1f140b9482cbad8eb700789cfd
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-09 14:29:49 +1000

    fix map branding

--- plugins/branding.py
+++ plugins/branding.py
@@ -4,9 +4,9 @@ import minqlx
 
 class branding(minqlx.Plugin):
     def __init__(self):
-        self.add_hook("map", self.brand_map)
+        self.add_hook("new_game", self.brand_map)
       
-    def brand_map(self, map, factory):
+    def brand_map(self):
       minqlx.set_configstring(3, "^4The Purgery^7")
       minqlx.set_configstring(678, "Sponsored by TomTec Solutions")
       minqlx.set_configstring(679, "Visit our IRC channel on QuakeNet, ^4#thepurgery^7.")
