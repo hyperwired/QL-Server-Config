277f0c75528bda05eadbc3489c7456c7fb67dae0
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-11 04:09:12 +1000

    added clean_name filter

--- plugins/fun.py
+++ plugins/fun.py
@@ -8,7 +8,8 @@ class fun(minqlx.Plugin):
         self.add_command("penislength", self.cmd_penlen) # Junkyard requested
  
     def cmd_penlen(self, player, msg, channel):
-        if player.clean_name == "Zeobyte":
+        playerName = player.clean_name
+        if playerName.lower() == "zeobyte":
             channel.reply("^7{}^7's penis length: ^412 inches^7".format(player))
         else:
             channel.reply("^7{}^7's penis length: ^4{} inch(es)^7".format(player, randint(0,11)))
