ec3c4556ba8e5dca7a9c674f2c1e9eeb5df7f37e
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-11 03:50:01 +1000

    added the 'fun' plugin to the server.cfg

--- plugins/fun.py
+++ plugins/fun.py
@@ -6,7 +6,9 @@ from random import randint
 class fun(minqlx.Plugin):
     def __init__(self):
         self.add_command("penislength", self.cmd_penlen) # Junkyard requested
-
-
+ 
     def cmd_penlen(self, player, msg, channel):
-        channel.reply("^7{}^7's penis length: ^4{} inch(es)^7".format(player, randint(0,12)))
+        if player.clean_name == "Zeobyte":
+            channel.reply("^7{}^7's penis length: ^412 inches^7".format(player))
+        else:
+            channel.reply("^7{}^7's penis length: ^4{} inch(es)^7".format(player, randint(0,11)))
