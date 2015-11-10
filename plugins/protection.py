7b08203fdbcff9b7ac69dfbf3d86e817703d95dc
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-11 00:42:45 +1000

    added the 'protection' plugin
    This plugin prohibits kicking myself, or merozollo, the other server
    administrator.

---
+++ plugins/protection.py
@@ -0,0 +1,25 @@
+# This has been created by TomTec Solutions
+
+import minqlx
+
+class protection(minqlx.Plugin):
+    def __init__(self):
+        self.add_hook("vote_called", self.handle_vote_called)
+
+
+    def handle_vote_called(self, caller, vote, args):
+        if vote.lower() == "kick":
+
+            if args == "zeobyte":
+                caller.tell("^7Voting to kick the server owner is prohibited. This incident will be logged.")
+                return minqlx.RET_STOP_ALL
+            
+            if args == "merozollo":
+                caller.tell("^7Voting to kick a server administrator is prohibited. This incident will be logged.")
+                return minqlx.RET_STOP_ALL
+
+
+        if vote.lower() == "clientkick":
+
+            caller.tell("^4clientkick^7 is disabled on this server. Use ^4kick^7 instead.")
+            return minqlx.RET_STOP_ALL
