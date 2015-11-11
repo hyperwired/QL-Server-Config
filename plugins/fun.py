621da8d1969852d52a6cf5904aea88a69ae6e46f
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-11 20:39:12 +1000

    added the 'poke' command

--- plugins/fun.py
+++ plugins/fun.py
@@ -1,15 +1,29 @@
 # This has been created by TomTec Solutions
-
 import minqlx
 from random import randint
 
 class fun(minqlx.Plugin):
     def __init__(self):
         self.add_command("penislength", self.cmd_penlen) # Junkyard requested
+        self.add_command("poke", self.cmd_poke, usage="<text>") # Merozollo requested
  
     def cmd_penlen(self, player, msg, channel):
         playerName = player.clean_name
         if playerName.lower() == "zeobyte":
             channel.reply("^7{}^7's penis length: ^412 inches^7".format(player))
         else:
-            channel.reply("^7{}^7's penis length: ^4{} inch(es)^7".format(player, randint(0,11)))
+            randNum = randint(0,11)
+            if randNum == 0:
+                channel.reply("^7{}^7's penis length: ^40 inches (inverted!)^7".format(player))
+            else:
+                channel.reply("^7{}^7's penis length: ^4{} inch(es)^7".format(player, randNum))
+
+
+    def cmd_poke(self, player, msg, channel):
+        if len(msg) < 2:
+            return minqlx.RET_USAGE
+
+        displayText = " ".join(msg[1:])
+        
+        minqlx.console_command("cp ^7WAKE UP, {}^7!".format(displayText))
+        self.play_sound("sound/world/klaxon2.wav")
