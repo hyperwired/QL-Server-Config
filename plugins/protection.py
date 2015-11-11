295e9a2e81434c44fb1d21c33247a2b7f9ec896c
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-11 10:04:26 +1000

    update protection.py

--- plugins/protection.py
+++ plugins/protection.py
@@ -9,17 +9,19 @@ class protection(minqlx.Plugin):
 
     def handle_vote_called(self, caller, vote, args):
         if vote.lower() == "kick":
-
-            if args.lower() == "zeobyte":
+    
+            playerName = args.lower()
+            
+            if playerName == "zeobyte":
                 caller.tell("^7Voting to kick the server owner is prohibited. This incident has been recorded.")
                 return minqlx.RET_STOP_ALL
             
-            if args.lower() == "merozollo":
+            if playerName == "merozollo":
                 caller.tell("^7Voting to kick a server administrator is prohibited. This incident has been recorded.")
                 return minqlx.RET_STOP_ALL
 
 
         if vote.lower() == "clientkick":
 
-            caller.tell("^4clientkick^7 is disabled on this server. Use ^4kick^7 instead.")
-            return minqlx.RET_STOP_ALL
+            playerID = args.lower()
+            # no need to block this anymore, if you're A, can't be clientkicked.
