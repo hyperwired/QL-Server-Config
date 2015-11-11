d71461e2e60e5f11e618be64e61ae88cbd2e9abb
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-11 17:49:21 +1000

    add a message in the middle of the screen

--- plugins/branding.py
+++ plugins/branding.py
@@ -5,8 +5,12 @@ import minqlx
 class branding(minqlx.Plugin):
     def __init__(self):
         self.add_hook("new_game", self.brand_map)
+        self.add_hook("game_countdown", self.sponsor_message)
       
     def brand_map(self):
-      minqlx.set_configstring(3, "^4The Purgery^7")
-      minqlx.set_configstring(678, "Sponsored by TomTec Solutions")
-      minqlx.set_configstring(679, "Visit our IRC channel on QuakeNet, ^4#thepurgery^7.")
+        minqlx.set_configstring(3, "^4The Purgery^7")
+        minqlx.set_configstring(678, "Sponsored by TomTec Solutions")
+        minqlx.set_configstring(679, "Visit our IRC channel on QuakeNet, ^4#thepurgery^7.")
+
+    def sponsor_message(self):
+        minqlx.send_server_command(None, "cp \"^4The Purgery\n^7Sponsored by TomTec Solutions^7\"\n")
