935525f3a78cb943c04f1f055150397080b4e153
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-14 00:03:20 +1000

    added a requested command

--- plugins/fun.py
+++ plugins/fun.py
@@ -7,6 +7,7 @@ from random import randint
 class fun(minqlx.Plugin):
     def __init__(self):
         self.add_command("penislength", self.cmd_penlen) # Junkyard requested
+        self.add_command("vaginadepth", self.cmd_vagdep) # Junkyard requested
         self.add_command("poke", self.cmd_poke, usage="<name>") # Merozollo requested
  
     def cmd_penlen(self, player, msg, channel):
@@ -20,6 +21,16 @@ class fun(minqlx.Plugin):
             else:
                 channel.reply("^7{}^7's penis length: ^4{} inch(es)^7".format(player, randNum))
 
+    def cmd_vagdep(self, player, msg, channel):
+        playerName = player.clean_name
+        if playerName.lower() == "zeobyte":
+            channel.reply("^7{}^7's vaginial depth: ^4-12 inches^7".format(player))
+        else:
+            randNum = randint(0,11)
+            if randNum == 0:
+                channel.reply("^7{}^7's vaginial depth: ^40 inches (are you trans?)^7".format(player))
+            else:
+                channel.reply("^7{}^7's vaginial depth: ^4{} inch(es)^7".format(player, randNum))
 
     def cmd_poke(self, player, msg, channel):
         if len(msg) < 2:
