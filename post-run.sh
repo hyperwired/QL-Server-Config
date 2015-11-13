444ade0284c2afa404641d112058fabd9d6dc32a
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-14 03:05:55 +1000

    removed patchcode

--- post-run.sh
+++ post-run.sh
@@ -2,8 +2,6 @@
 # Post-run script
 
 # Starting supervisord again now the new inet-based config is in place.
-/usr/local/bin/supervisord
-sleep 300
 
 echo "Running 'quakeupdate.sh'..."
 bash ~/quakeupdate.sh
