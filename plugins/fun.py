8c889aef2a78fb34419f723b36d0af756b79a309
Thomas Jones <thomas@tomtecsolutions.com>
2015-11-17 03:02:34 +1000

    updated plugin

--- plugins/fun.py
+++ plugins/fun.py
@@ -9,6 +9,7 @@ class fun(minqlx.Plugin):
         self.add_command("penislength", self.cmd_penlen) # Junkyard requested
         self.add_command(("vaginadepth", "vaginaldepth", "vaginialdepth"), self.cmd_vagdep) # Junkyard requested
         self.add_command(("msg", "message"), self.cmd_screenmessage, 1, usage="<text>") # Merozollo requested
+        self.add_command(("breastsize", "cupsize", "brasize", "boobsize"), self.cmd_boobsize) # 0regonn requested
  
     def cmd_penlen(self, player, msg, channel):
         playerName = player.clean_name
@@ -26,12 +27,32 @@ class fun(minqlx.Plugin):
         if playerName.lower() == "zeobyte":
             channel.reply("^7{}^7's vaginial depth: ^4-12 inches^7".format(player))
         else:
-            randNum = randint(0,11)
+            randNum = randint(0,4)
             if randNum == 0:
                 channel.reply("^7{}^7's vaginial depth: ^40 inches (are you trans?)^7".format(player))
             else:
                 channel.reply("^7{}^7's vaginial depth: ^4{} inch(es)^7".format(player, randNum))
 
+    def cmd_boobsize(self, player, msg, channel):
+        playerName = player.clean_name
+        if playerName.lower() == "zeobyte":
+            channel.reply("^7{}^7's cup size: ^4Stop asking, you're male!".format(playerName))
+        else:
+            if randNum == 0:
+                cupSize = "A"
+            elif randNum == 1:
+                cupSize = "B"
+            elif randNum == 2:
+                cupSize = "C"
+            elif randNum == 3:
+                cupSize = "D"
+            elif randNum == 4:
+                cupSize = "DD"
+            elif randNum == 5:
+                cupSize = "Z (discount wheelbarrow at Bunnings!)"
+
+            channel.reply("^7{}^7's cup size: ^4{}^7".format(playerName, cupSize))
+           
     def cmd_screenmessage(self, player, msg, channel):
         if len(msg) < 2:
             return minqlx.RET_USAGE
