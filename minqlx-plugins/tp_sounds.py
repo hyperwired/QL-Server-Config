3a5ab53ae0bc9d55051950fc2013fd7d9c331f1c
Thomas Jones <thomas@tomtecsolutions.com>
2015-12-07 18:45:34 +1000

    add sound queueing plugin for testing out sound packs

---
+++ minqlx-plugins/tp_sounds.py
@@ -0,0 +1,23 @@
+# This file is part of the Quake Live server implementation by TomTec Solutions. Do not copy or redistribute or link to this file without the emailed consent of Thomas Jones (thomas@tomtecsolutions.com).
+
+import minqlx
+
+class tp_sounds(minqlx.Plugin):
+    def __init__(self):
+        self.add_hook("map", self.map_load)
+        self.add_command("queueq3a", self.cmd_queueSoundPack, 5)
+        self.add_command("unqueueq3a", self.cmd_unqueueSoundPack, 5)
+
+        self.set_cvar_once("qlx_queueSoundPack", "0")
+        
+    def map_load(self, mapname, factory):
+        if self.get_cvar("qlx_queueSoundPack") == "1":
+            self.game.steamworks_items += [570745980]
+
+    def cmd_queueSoundPack(self, player, msg, channel):
+        self.set_cvar("qlx_queueSoundPack", "1")
+        channel.reply("Quake III Arena: Sound pack queued.")
+                        
+    def cmd_unqueueSoundPack(self, player, msg, channel):
+        self.set_cvar("qlx_queueSoundPack", "0")
+        channel.reply("Quake III Arena: Sound pack unqueued.")
