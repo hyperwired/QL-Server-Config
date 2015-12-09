ac455da352745e1233161bdf508a7badfc8ee641
Thomas Jones <thomas@tomtecsolutions.com>
2015-12-09 13:16:45 +1000

    change workshop ids and add a purgery soundpack

--- minqlx-plugins/tp_sounds.py
+++ minqlx-plugins/tp_sounds.py
@@ -2,22 +2,38 @@
 
 import minqlx
 
+queue_q3a = False
+queue_tp = False
+
 class tp_sounds(minqlx.Plugin):
     def __init__(self):
         self.add_hook("map", self.map_load)
-        self.add_command("queueq3a", self.cmd_queueSoundPack, 5)
-        self.add_command("unqueueq3a", self.cmd_unqueueSoundPack, 5)
+        self.add_command("queue_q3a", self.cmd_queueSoundPack_q3a, 5)
+        self.add_command("unqueue_q3a", self.cmd_unqueueSoundPack_q3a, 5)
 
-        self.set_cvar_once("qlx_queueSoundPack", "0")
+        self.add_command("queue_tp", self.cmd_queueSoundPack_tp, 5)
+        self.add_command("unqueue_tp", self.cmd_unqueueSoundPack_tp, 5)
+
         
     def map_load(self, mapname, factory):
-        if self.get_cvar("qlx_queueSoundPack") == "1":
-            self.game.steamworks_items += [571860199]
+        if queue_tp == True:
+            self.game.steamworks_items += [571878211]
 
-    def cmd_queueSoundPack(self, player, msg, channel):
-        self.set_cvar("qlx_queueSoundPack", "1")
+        if queue_q3a == True:
+            self.game.steamworks_items += [571878681]
+
+    def cmd_queueSoundPack_q3a(self, player, msg, channel):
+        queue_q3a = True
         channel.reply("^1Quake III Arena^7: Sound pack queued. Reload map to reference steamworks item to clients.")
                         
-    def cmd_unqueueSoundPack(self, player, msg, channel):
-        self.set_cvar("qlx_queueSoundPack", "0")
+    def cmd_unqueueSoundPack_q3a(self, player, msg, channel):
+        queue_q3a = False
         channel.reply("^1Quake III Arena^7: Sound pack unqueued.")
+
+    def cmd_queueSoundPack_tp(self, player, msg, channel):
+        queue_tp = True
+        channel.reply("^1The Purgery - Sound Pack^7: Sound pack queued. Reload map to reference steamworks item to clients.")
+                        
+    def cmd_unqueueSoundPack_tp(self, player, msg, channel):
+        queue_tp = False
+        channel.reply("^1The Purgery - Sound Pack^7: Sound pack unqueued.")
