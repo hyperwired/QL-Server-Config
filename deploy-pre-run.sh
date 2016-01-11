2dda675b3c64ef636e00e4014302312bf3a00dda
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-12 05:12:13 +1000

    remove pre-run code

--- deploy-pre-run.sh
+++ deploy-pre-run.sh
@@ -2,7 +2,4 @@
 # This file is part of the Quake Live server implementation by TomTec Solutions. Do not copy or redistribute or link to this file without the emailed consent of Thomas Jones (thomas@tomtecsolutions.com).
 # Pre-run script for 'deploy.sh'
 
-rm -rf ~/steamcmd/steamapps/common/qlds/minqlx.zip
-mkdir ~/steamcmd/steamapps/common/qlds/minqlx
-
 exit 0
