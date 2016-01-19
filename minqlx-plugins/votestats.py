c3ef760e1f097b128bdcb8693e9cce54c7582e03
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-19 22:19:36 +1000

    mirror the version in the Quake-Live repo

--- minqlx-plugins/votestats.py
+++ minqlx-plugins/votestats.py
@@ -2,6 +2,10 @@
 # Created by Thomas Jones on 01/01/2016 - thomas@tomtecsolutions.com
 # votestats.py - a minqlx plugin to show who votes yes or no in-game/vote results.
 
+"""
+If you want to re-privatise votes, set the following cvar to 1: qlx_privatiseVotes
+"""
+
 import minqlx
 
 class votestats(minqlx.Plugin):
@@ -10,12 +14,16 @@ class votestats(minqlx.Plugin):
         self.add_hook("vote_ended", self.handle_vote_ended, priority=minqlx.PRI_LOWEST)
         
         self.add_command("tomtec_versions", self.cmd_showversion)
-        
+
+        self.set_cvar_once("qlx_privatiseVotes", "0")
         self.plugin_version = "1.5"
 
         self.has_voted = []
 
     def process_vote(self, player, yes):
+        if (self.get_cvar("qlx_privatiseVotes", bool)):
+            return
+        
         if player in self.has_voted:
             return
         
@@ -25,10 +33,10 @@ class votestats(minqlx.Plugin):
             word = "^1no"
             
         self.msg("{}^7 voted {}^7.".format(player.name, word))
+        self.has_voted.append(player)
 
     def handle_vote_ended(self, votes, vote, args, passed):
         self.has_voted = []
-        
         self.msg("Vote results: ^2{}^7 - ^1{}^7.".format(*votes))
         
         if passed:
@@ -37,7 +45,7 @@ class votestats(minqlx.Plugin):
                 if len(changingToMapAndMode) > 1:
                     theMsg = "The map is changing to ^4{}^7, with new game type ^4{}^7.".format(changingToMapAndMode[0], changingToMapAndMode[1])
                 else:
-                    theMsg = "The map is changing to ^4{}^7, with same game type (^4{}^7).".format(changingToMapAndMode[0], self.game.factory)
+                    theMsg = "The map is changing to ^4{}^7.".format(changingToMapAndMode[0])
 
                 self.msg(theMsg)
     
