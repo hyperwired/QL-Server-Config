643f4265c0612efb262cce5d054be994af95d326
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-21 19:10:56 +1000

    change the plugin to be player-responsive only

--- minqlx-plugins/locations.py
+++ minqlx-plugins/locations.py
@@ -8,12 +8,12 @@ import threading
 
 class locations(minqlx.Plugin):
     def __init__(self):
-        self.add_command(("loc", "location"), self.cmd_location, usage="<id>")
+        self.add_command(("loc", "location"), self.cmd_location, 5, usage="<id>")
         self.add_command("tomtec_versions", self.cmd_showversion)
 
-        self.plugin_version = "1.0"
+        self.plugin_version = "1.1"
 
-    @minqlx.thread    
+    @minqlx.thread
     def cmd_location(self, player, msg, channel):
         if len(msg) < 2:
             return minqlx.RET_USAGE
@@ -22,15 +22,16 @@ class locations(minqlx.Plugin):
             player_ip = self.player(int(msg[1])).ip
             player_name = self.player(int(msg[1])).name
         except:
-            channel.reply("^1Invalid Client ID.^7 Enter a valid client ID to see their approximate location.")
+            player.tell("^1Invalid Client ID.^7 Enter a valid client ID to see their approximate location.")
             return
     
         ipData = requests.get('http://freegeoip.net/json/{}'.format(player_ip), stream=True)
         ipData = str(ipData.text)
         ipDataParsed = json.loads(ipData)
 
-        channel.reply("{}^7's approximate location details:\n    Country Code: {}\n    Country Name: {}\n    Region Code: {}\n    Region Name: {}\n    City: {}\n    Post Code: {}\n    Time Zone: {}".format(player_name, ipDataParsed['country_code'], ipDataParsed['country_name'], ipDataParsed['region_code'], ipDataParsed['region_name'], ipDataParsed['city'], ipDataParsed['zip_code'], ipDataParsed['time_zone']))
-        return
+        player.tell("{}^7's approximate location details:\n    Country Code: {}\n    Country Name: {}\n    Region Code: {}\n    Region Name: {}\n    City: {}\n    Post Code: {}\n    Time Zone: {}".format(player_name, ipDataParsed['country_code'], ipDataParsed['country_name'], ipDataParsed['region_code'], ipDataParsed['region_name'], ipDataParsed['city'], ipDataParsed['zip_code'], ipDataParsed['time_zone']))
+
+        return minqlx.RET_STOP_ALL
     
     def cmd_showversion(self, player, msg, channel):
         channel.reply("^4locations.py^7 - version {}, created by Thomas Jones on 21/01/2016.".format(self.plugin_version))
