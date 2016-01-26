f5f93ca185601a28c99231abda427efe08d024b2
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-27 06:03:38 +1000

    change wording of cycling messages

--- minqlx-plugins/maintenance.py
+++ minqlx-plugins/maintenance.py
@@ -9,17 +9,24 @@ import minqlx
 class maintenance(minqlx.Plugin):
     def __init__(self):
         self.add_hook("player_connect", self.handle_player_connected)
-        self.msg("^1Maintenance plugin has been loaded.^7")
+        self.msg("^13:00am AEST:^7 Servers are going down for routine maintenance/updates.^7")
+        minqlx.unload_plugin("tomtec_logic")
+        minqlx.unload_plugin("irc")
+        for p in self.players():
+            minqlx.client_command(p.id, "disconnect")
         minqlx.console_command("map campgrounds ffa")
         self.counter = 0
 
     def handle_player_connected(self, player):
         if self.counter == 0:
             self.counter = 1
-            return "TomTec Solutions servers are currently down for maintenance."
+            return "TomTec Solutions servers are currently down for routine updates."
         elif self.counter == 1:
             self.counter = 2
-            return "You'll be back in the game within a few short minutes."
+            return "Routine maintenance/updates occurs at 3:00am AEST daily."
         elif self.counter == 2:
+            self.counter = 3
+            return "If the servers are down at any other time, please contact Purger as soon as possible."
+        elif self.counter == 3:
             self.counter = 0
-            return "Please wait patiently."
+            return "Please wait patiently, the server will be online again within a few minutes."
