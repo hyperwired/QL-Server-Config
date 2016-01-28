32105c43e0695bbd6b91cf89c4da9f182c33695e
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-29 04:00:56 +1000

    hopefully gracefully reload the clients this way

--- minqlx-plugins/maintenance.py
+++ minqlx-plugins/maintenance.py
@@ -9,11 +9,11 @@ import minqlx
 class maintenance(minqlx.Plugin):
     def __init__(self):
         self.add_hook("player_connect", self.handle_player_connected)
-        self.msg("^13:00am AEST:^7 Servers are going down for routine maintenance/updates.^7")
+        self.msg("^1Servers are going down for routine maintenance/updates.^7")
         minqlx.unload_plugin("tomtec_logic")
         minqlx.unload_plugin("irc")
         for p in self.players():
-            minqlx.client_command(p.id, "disconnect")
+            minqlx.send_server_command(p.id, "cs 0 0")
         minqlx.console_command("map campgrounds ffa")
         self.counter = 0
 
@@ -23,10 +23,7 @@ class maintenance(minqlx.Plugin):
             return "TomTec Solutions servers are currently down for routine updates."
         elif self.counter == 1:
             self.counter = 2
-            return "Routine maintenance/updates occurs at 3:00am AEST daily."
+            return "Routine maintenance/updates usually occurs at 3:00am AEST daily."
         elif self.counter == 2:
-            self.counter = 3
-            return "If the servers are down at any other time, please contact Purger as soon as possible."
-        elif self.counter == 3:
             self.counter = 0
             return "Please wait patiently, the server will be online again within a few minutes."
