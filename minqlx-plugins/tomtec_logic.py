ff3088f22ab6cb0f98fd4c48a893143747512d3f
Thomas Jones <thomas@tomtecsolutions.com>
2016-03-18 00:30:56 +1000

    maybe this'll fix it

--- minqlx-plugins/tomtec_logic.py
+++ minqlx-plugins/tomtec_logic.py
@@ -50,11 +50,11 @@ class tomtec_logic(minqlx.Plugin):
         if (datetime.datetime.now().month == 3) and (datetime.datetime.now().day == 18):
             # It's Purger's birthday.
             self.purgersBirthday = True
-            self.set_cvar("purgersBirthday", "1", 68)
+            self.set_cvar("purgersBirthday", "1", flags=68)
             self.set_cvar("qlx_connectMessage", "^7It's Pur^4g^7er's Birthday!")
             self.set_cvar("qlx_countdownMessage", "^7It's Pur^4g^7er's Birthday!")
         else:
-            self.set_cvar("purgersBirthday", "0", 68)
+            self.set_cvar("purgersBirthday", "0", flags=68)
             
         if self.get_cvar("qlx_strictVql", bool):
             minqlx.load_plugin("strictvql")
