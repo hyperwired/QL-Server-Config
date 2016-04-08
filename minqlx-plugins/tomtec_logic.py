842248000619e36ba0b4d343896004c776c7f0f6
Thomas Jones <thomas@tomtecsolutions.com>
2016-04-08 15:04:48 +1000

    add a resolver for oxodm102_b1

--- minqlx-plugins/tomtec_logic.py
+++ minqlx-plugins/tomtec_logic.py
@@ -240,6 +240,9 @@ class tomtec_logic(minqlx.Plugin):
 
                 return minqlx.RET_STOP_ALL
 
+            if args.lower() in "rustcampgrounds":
+                return minqlx.RET_STOP_ALL
+
         if (vote.lower() == "kick") or (vote.lower() == "clientkick"):
             if len(args.split()) < 1:
                 return minqlx.RET_STOP
