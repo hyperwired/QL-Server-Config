8f9d6635689bdefdce926e073371a7db734396e0
Thomas Jones <thomas@tomtecsolutions.com>
2016-04-12 21:05:29 +1000

    change topfield

--- scripts/quakestart.sh
+++ scripts/quakestart.sh
@@ -26,7 +26,7 @@ export qPurgeryStart="$qPathToMinqlxStartScript \
 +set qlx_motdSound 0 \
 +set g_inactivity 120 \
 +set qlx_serverBrandName $qPurgeryServerTitle \
-+set qlx_serverBrandTopField ^7Sponsored by ^5TomTec Solutions^7. Visit our Wiki at ^2thepurgery.com^7. Change the server configuration here: ^2github.com/TomTec-Solutions/QL-Server-Config^7. \
++set qlx_serverBrandTopField ^7Sponsored by ^5TomTec Solutions^7. Visit the forums at ^2forum.thepurgery.com^7. \
 +set qlx_serverBrandBottomField ^5www.4seasonsgaming.com^7, home to the Australian and New Zealand Quake community. \
 +set qlx_loadedMessage Welcome to ^4The Purgery^7 \
 +set qlx_countdownMessage ^5Good luck, and have fun.^7 \
