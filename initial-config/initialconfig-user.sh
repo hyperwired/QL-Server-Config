f6caea63c5dba0785ee4bafe50ab5abe3cdc6f03
Thomas Jones <thomas@tomtecsolutions.com>
2016-04-13 13:21:35 +1000

    add a missing bit of the path

--- initial-config/initialconfig-user.sh
+++ initial-config/initialconfig-user.sh
@@ -8,8 +8,8 @@ mkdir ~/steamcmd; cd ~/steamcmd; wget https://steamcdn-a.akamaihd.net/client/ins
 ./steamcmd.sh +login anonymous +force_install_dir /home/qlserver/steamcmd/steamapps/common/qlds/ +app_update 349090 +quit
 
 # make directories that deploy.sh'll complain about later on if they're not there
-mkdir /home/qlserver/steamcmd/common/qlds/minqlx-plugins
-mkdir /home/qlserver/steamcmd/common/qlds/baseq3/scripts
+mkdir /home/qlserver/steamcmd/steamapps/common/qlds/minqlx-plugins
+mkdir /home/qlserver/steamcmd/steamapps/common/qlds/baseq3/scripts
 
 # DON'T FORGET TO PUT THE LOCALCONFIG TEXT FILES WHERE THEY NEED TO GO
 
