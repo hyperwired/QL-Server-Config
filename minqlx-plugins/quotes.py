ef6f272f49ee2441747774db44df7a2e3fcd1ddb
Thomas Jones <thomas@tomtecsolutions.com>
2016-04-14 20:11:08 +1000

    rename connect_quotes to quotes

--- minqlx-plugins/quotes.py
+++ minqlx-plugins/quotes.py
@@ -25,14 +25,15 @@ QUOTES = [
 
 import minqlx
 from random import randint
-class connect_quotes(minqlx.Plugin):
+class quotes(minqlx.Plugin):
     def __init__(self):
         self.add_hook("player_connect", self.handle_player_connect)
         self.add_hook("player_loaded", self.handle_player_loaded)
 
         self.add_command("tomtec_versions", self.cmd_showversion)
+        self.add_command("quote", self.cmd_quote)
         
-        self.plugin_version = "1.0"
+        self.plugin_version = "1.1"
 
         self.playerConnectedYetList = []
         
@@ -50,5 +51,10 @@ class connect_quotes(minqlx.Plugin):
         except:
             return
 
+    def cmd_quote(self, player, msg, channel):
+        randomNumber = randint(0, (len(QUOTES) - 1))
+        randomQuote = QUOTES[randomNumber]
+        channel.reply(randomQuote)
+        
     def cmd_showversion(self, player, msg, channel):
-        channel.reply("^4connect_quotes.py^7 - version {}, created by Thomas Jones on 08/04/2016.".format(self.plugin_version))
+        channel.reply("^4quotes.py^7 - version {}, created by Thomas Jones on 08/04/2016.".format(self.plugin_version))
