421372ecb34d7bad5fb125d71ead62b17f87b524
Thomas Jones <thomas@tomtecsolutions.com>
2016-08-21 18:12:31 +1000

    add damn time module

--- minqlx-plugins/tp_fun.py
+++ minqlx-plugins/tp_fun.py
@@ -1,7 +1,7 @@
 # This file is part of the Quake Live server implementation by TomTec Solutions. Do not copy or redistribute or link to this file without the emailed consent of Thomas Jones (thomas@tomtecsolutions.com).
 # This file contains community ideas that may be found to be in bad taste. These views do not represent the views of TomTec Solutions
 
-import minqlx, re
+import minqlx, re, time
 from random import randint
 
 _re_purger = re.compile(r"^purger\W?$", flags=re.IGNORECASE)
